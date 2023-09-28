535b0ca5fd06481b9abb38d55e6a0e5cdb40de5c
jdmunoz21 <jdmunoz21@students.desu.edu>
2023-09-28 21:19:44 +0000

    second

--- lab1.c
+++ lab1.c
@@ -1,12 +1,33 @@
 /*
- * Implement your solution in thi file
+ * Implement your solution in this file
  *
  */
 
-char* readString(char* fileName){
+#include <stdio.h>
+#include <string.h>
 
+char* readString(char* fileName){
+    
 }
 
 char* mysteryExplode(const char* str){
     int lenOfString = 0;
+
+    for (int i = 0; i < strlen(str) + 1; i++){
+        lenOfString += i;
+    }
+
+    char substring [strlen(str)];
+    char explodedString[lenOfString];
+    substring[0] = '\0';
+    explodedString[0] = '\0';
+
+    for (int n = 0; n < strlen(str); n++){
+        char substringToAdd = str[n];
+        substring[n] = str[n];
+        strcat(explodedString, substring);
+    }
+
+    printf("%s\n", explodedString);
+    return 0;
 }
