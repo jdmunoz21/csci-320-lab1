7645f3f4464b320452e63a8b57852d32d3a767e1
jdmunoz21 <jdmunoz21@students.desu.edu>
2023-10-06 19:43:44 +0000

    eigth and final

--- lab1.c
+++ lab1.c
@@ -12,6 +12,7 @@ char* readString(char* fileName){
 
     FILE *f = fopen(fileName, "r");
     fgets(string, 100, f);
+    string[strcspn(string, "\n")] = '\0';
     fclose(f);
 
     return strdup(string);
@@ -29,7 +30,7 @@ char* mysteryExplode(const char* str){
     substring[0] = '\0';
     explodedString[0] = '\0';
 
-    for (int n = 0; n < strlen(str) - 1; n++){
+    for (int n = 0; n < strlen(str); n++){
         char substringToAdd[1]; 
         substringToAdd[0] = str[n];
         strcat(substring, substringToAdd);
